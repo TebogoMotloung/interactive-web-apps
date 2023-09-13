49fcb06e51a829221ebab5fd1912609cf247abc6
TebogoMotloung <tebohotmotloung98@gmai.com>
2023-09-13 21:44:34 +0200

    Mod codes for each Challenge

---
+++ IWA2/Challenge - 3.js
@@ -0,0 +1,14 @@
+/*
+It is important to show the following message in order to:
+- to assure users
+- to scare hackers
+- to impress investors
+*/
+const warn = 'Security scan starting'
+
+console.log(warn)
+
+/* It is important to let user know when they can close the page  */
+
+const info = 'Please wait for scan to complete before closing the browser.'
+console.log(info) 
