efc740c6451800c860d74d6dacada31417f55edc
TebogoMotloung <140751044+TebogoMotloung@users.noreply.github.com>
2023-09-26 22:08:02 +0200

    Add files via upload

---
+++ IWA4/scripts.js
@@ -0,0 +1,45 @@
+const date= '2050'
+let status=  'parent'
+let count = '0'
+
+if (date== 2050) {
+
+	console.log("January", 'New Year’s Day')
+	console.log("March", 'Human Rights Day')
+	console.log("April", 'Family Day')
+	console.log("April", 'Freedom Day')
+	console.log("August", 'Women’s Day')
+	console.log( "September", 'Heritage Day')
+	console.log("December", 'Day of Reconciliation')
+    console.log("December", 'Christmas Day')
+	console.log("December", 'Day of Goodwill')
+	let count = 9
+
+console.log('Your status is:', status)
+console.log('The year is:', date)
+console.log('The total holidays is:', count)
+
+
+    if (true){
+        let status='student'
+
+        console.log("January", 'New Year’s Day')
+	console.log("March", 'Human Rights Day')
+	console.log("April", 'Family Day')
+	console.log("April", 'Freedom Day')
+    console.log('June', 'Youth Day')
+	console.log("August", 'Women’s Day')
+	console.log( "September", 'Heritage Day')
+	console.log("December", 'Day of Reconciliation')
+	console.log("December", 'Day of Goodwill')
+	count = 9
+
+    }
+
+    console.log('Your status is:', status)
+console.log('The year is:', date)
+console.log('The total holidays is:', count)
+}
+ 
+
+
---
+++ IWA5/scripts.js
@@ -0,0 +1,32 @@
+FREE_SHIPPING = 'Free shipping only applies to single customer orders';
+BANNED_WARNING = 'Unfortunately we do not ship to your country of residence';
+NONE_SELECTED = '0';
+
+const customers = '1';
+let location = 'RSA' ;
+
+if (location = 'RSA') {
+     shipping === 400 && currency === 'R'
+     } else if (location = 'NAM') {
+         shipping = 600 && currency === '$'
+        }else 
+          shipping = 800 && currency ==='$'
+
+shoes = 300 * 1
+toys - 100 * 5
+shirts = 150 * 'NONE_SELECTED'
+batteries = 35 * 2
+pens = 5 * 'NONE_SELECTED' 
+
+
+if (location = 'NAM' || 'RSA' && customers < 2) {
+    shoes + batteries + pens + shirts > 1000 || 60
+    } calcShipping
+		
+		
+if (shipping = 0 && customers === 1) 
+ console.log(FREE_SHIPPING) 
+
+location = 'NK' ? console.log(BANNED_WARNING) : console.log('price', currency, shoes + batteries + pens + shirts)
+
+
