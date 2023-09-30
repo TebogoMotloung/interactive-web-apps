04bc07ba06d44a3b99709a83e1663f7dc49dec9a
TebogoMotloung <tebohotmotloung98@gmai.com>
2023-09-30 15:40:12 +0200

    mod

---
+++ IWA6/scripts1.js
@@ -0,0 +1,20 @@
+const primaryPhone = 'O748105141'
+const secondaryPhone = '0219131568'
+
+// Only change below this line
+  parseInt(secondaryPhone)
+
+const primaryValid = typeof number == primaryPhone
+const secondaryValid = typeof 219131568  == secondaryPhone
+
+
+
+
+
+
+console.log('Primary phone is valid numerical string:', primaryValid) 
+ console.log('Secondary phone is valid numerical string:', secondaryValid )
+
+
+
+
