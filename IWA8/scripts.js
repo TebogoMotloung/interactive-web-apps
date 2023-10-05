4300d6cc7f8778e9342121f79242db4b4c692273
TebogoMotloung <tebohotmotloung98@gmai.com>
2023-10-05 13:56:23 +0200

    mod

---
+++ IWA8/scripts.js
@@ -0,0 +1,48 @@
+const leoName = 'Leo Musvaire'
+const leoNumber = '2'
+const leoStreet = 'Church St.'
+const leoPostal = '3105'
+const leoBalance = '-10'
+
+const sarahName = 'Sarah    '
+const sarahSurname = 'Kleinhans'
+const sarahBalance = '-4582.21000111'
+const sarahNumber = '13'
+const sarahStreet = 'William Close'
+const sarahPostal = '0310'
+
+// Only change below this line
+
+
+const leoResult = {
+    leo :{
+    name: leoName,
+    age: 24,
+	balance: leoBalance,
+	accessId: '47afb389-8014-4d0b-aff3-e40203d2107f',
+    },
+
+	address: {
+		number: leoNumber,
+		street: leoStreet,
+		postalCode: leoPostal,
+	},
+}
+
+const sarahResult = {
+    sarah : {
+	name: sarahName, 
+    surname: sarahSurname,
+	age: 62,
+	accessId: '6b279ae5-5657-4240-80e9-23f6b635f7a8',
+	balance: sarahBalance,
+    },
+	address: {
+		number: sarahNumber,
+		street: sarahStreet,
+		postalCode: sarahPostal,
+	},
+}
+
+console.log(leoResult)
+console.log(sarahResult)
