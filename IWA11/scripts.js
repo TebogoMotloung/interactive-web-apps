1915b1a5e7dad34e707b82d227c6f4b56dc9b5b7
TebogoMotloung <tebohotmotloung98@gmai.com>
2023-10-22 15:26:21 +0200

    mod

--- IWA11/scripts.js
+++ IWA11/scripts.js
@@ -1,34 +1,43 @@
 // script.js
 
-const oneRoot = document.querySelector('[data-key="order1"]');
+
+
+const oneRoot= document.querySelector('[data-key="order1"]')
 const oneBiscuits= document.querySelector('[data-biscuits="10"]');
-const oneDonuts =document.querySelector('[data-key="13"]');
-const onePancakes= document.querySelector('[data-key="0"]');
-const oneStatus= document.querySelector('[data-key="false"]');
+const oneDonuts =document.querySelector('[data-donuts="13"]');
+const onePancakes= document.querySelector('[data-pancakes="0"]');
+const oneStatus= document.querySelector('[data-delivered="false"]');
+
 
 const twoRoot = document.querySelector('[data-key="order2"]');
-const twoBiscuits= document.querySelector('[data-key="5"]');
-const twoDonuts =document.querySelector('[data-key="0"]');
-const twoPancakes= document.querySelector('[data-key="2"]');
-const twoStatus= document.querySelector('[data-key="false"]');
+const twoBiscuits= document.querySelector('[data-biscuit="5"]');
+const twoDonuts =document.querySelector('[data-donuts="0"]');
+const twoPancakes= document.querySelector('[data-pancakes="2"]');
+const twoStatus= document.querySelector('[data-delivered="false"]');
 
-const threeRoot = document.querySelector(order3);
-const threeBiscuits= document.querySelector(biscuits);
-const threeDonuts =document.querySelector(donuts);
-const threePancakes= document.querySelector(pancakes);
-const threeStatus= document.querySelector(status);
+const threeRoot = document.querySelector('[data-key="order3"]');
+const threeBiscuits= document.querySelector('[data-biscuits="12"]');
+const threeDonuts =document.querySelector('[data-donuts="11"]');
+const threePancakes= document.querySelector('[data-pancakes="15"]')
+const threeStatus= document.querySelector('[data-delivered="true"]');
 
-oneBiscuits= 1-root.biscuits,
-oneDonuts = 1-root.donuts,
-onePancakes = 1-root.pancakes,
-oneStatus= 1-root.status ? Delivered : Pending
 
-twoBiscuits= 2-root.biscuits,
-twoDonuts = 2-root.donuts,
-twoPancakes = 2-root.pancakes,
-twoStatus = 2-root.status ? Delivered : Pending
 
-threeBiscuits= 3-root.biscuits,
-threeDonuts = 3-root.donuts,
-threePancakes = 3-root.pancakes,
-threeStatus = 3-root.status ? Delivered : Pending
+
+
+// oneBiscuits && twoBiscuits && threeBiscuits === 
+console.log(oneBiscuits.innerText)
+
+// oneDonuts = 1-root.donuts,
+// onePancakes = 1-root.pancakes,
+// oneStatus= 1-root.status ? Delivered : Pending
+
+// twoBiscuits= 2-root.biscuits,
+// twoDonuts = 2-root.donuts,
+// twoPancakes = 2-root.pancakes,
+// twoStatus = 2-root.status ? Delivered : Pending
+
+// threeBiscuits= 3-root.biscuits,
+// threeDonuts = 3-root.donuts,
+// threePancakes = 3-root.pancakes,
+// threeStatus = 3-root.status ? Delivered : Pending
